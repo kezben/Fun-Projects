12faf67734154caa6dd5ea2b5c037b6af6f2ebc1
Kez <57690159+kezben@users.noreply.github.com>
2020-09-18 15:40:35 +1200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -12,7 +12,7 @@ headers = {
 
 url = "http://lottoresults.co.nz/lotto/archive"
 
-# open conncetions, read NZ Lotto archive webpage, close connection
+# open connections, read NZ Lotto archive webpage, close connection
 client = uo(url)
 webpage = client.read()
 client.close()
@@ -20,11 +20,27 @@ client.close()
 # html parsing
 webpage_soup = soup(webpage, "html.parser")
 
-# grabs each month from each year
-all_months = webpage_soup.findAll("div", {"class":"archive-box"})
+# grabs url for every month in every year and appends to an array
+extract_months = webpage_soup.findAll("ul", {"class":""})
+all_months = str(extract_months)
+end_urls = []
+i = 1
+for month in all_months.split('"'):
+    if (i % 2 == 0):
+        end_urls.append(month)
+    i+=1
 
+# base url with each month added links to each results page
+baseUrl = "http://lottoresults.co.nz"
 
-month = all_months[0].findAll("a", {"href":"/lotto/august-2020"})
+# arrays for each ball, bonus ball & power ball
+b1, b2, b3, b4, b5, b6 = [], [], [], [], [], []
+bb = []
+pb = []
 
-print(all_months)
-print(month)
+#for link in end_urls:
+current_link = (baseUrl + end_urls[0])
+client2 = uo(current_link)
+webpage2 = client2.read()
+client2.close()
+webpage2_soup = soup(webpage2, "html.parser")
